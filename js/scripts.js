29b136392a4c551a5a1bb98d8a46aa0dbda303af
Liz Par <LizPar@Lizs-MacBook-Air.local>
2015-02-11 13:07:08 -0500

    Adding Hover and cleaned up

--- js/scripts.js
+++ js/scripts.js
@@ -1,30 +1,22 @@
 $(document).ready(function(){
   //add list items
 
-        $('button').on('click', function(event) {
-                event.preventDefault();
-                var listEntry = $("input").val();
+  $('.add').on('click', function(event) {
+    event.preventDefault();
+    var listEntry = $("input").val();
+    $('#list').append('<li><button class="delete">&times;</button><span>' +listEntry+ '</span></li>');
+    $('input').val("");
+  });
   
-                $('#list').append('<li><button class="delete">&times;</button>' +listEntry+ '</li>');
-                $('input').val("");
-                });
-  
-  		//$('.delete').on('click', function(e) {
-  		//$(this).remove();
+  $('#list').on('click', '.delete', function(event){
+    event.preventDefault();
+    $(this).parent().remove();                                         
+    });
 
-  		//});
-        
-        $('.delete').on('click', function(e){
-          $(this).remove();                                           
-          });
+  	     
+  	$('#list').on('click', 'li span', function(e) {
+      $(this).toggleClass('linethrough');
 
-  	
-  		//$("#list").on('click',function(e) {
-        	//$("#this").fadeOut("slow");
+  	});
 
-  		//});
-  		
-
-
-
-        });
+});
